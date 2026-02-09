93aebf9afa6a66121e26a31627e26e22c0562bc7
pr-ruslan <r.priadko@ukr.net>
2026-02-09 13:08:56 +0200

    Migrations applied

--- social_media/settings.py
+++ social_media/settings.py
@@ -18,6 +18,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
