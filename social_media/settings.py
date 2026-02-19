5f8ce5f6777b7a4712572d61cd793e5798eb6647
Ruslan Priadko <r.priadko@ukr.net>
2026-02-19 15:38:07 +0200

    Prepared for postres migration

--- social_media/settings.py
+++ social_media/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = "social_media.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT", default="5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
